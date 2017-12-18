3746b28ab64236bfdb6cfdcd8907d5eef6164736
sriky <srikanthbm@gmail.com>
2017-12-18 13:45:51 -0800

    Feat: Add Behavior for the share FAB to hide upon scrolling down and show-up when scrolling up.

---
+++ app/src/main/java/com/sriky/materialreader/behavior/ScrollAwareFABBehavior.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 Srikanth Basappa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.sriky.materialreader.behavior;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import timber.log.Timber;
+
+/**
+ * Class to define the behavior of {@link FloatingActionButton}
+ */
+
+public class ScrollAwareFABBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull FloatingActionButton child,
+                                       @NonNull View directTargetChild,
+                                       @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                               @NonNull FloatingActionButton child,
+                               @NonNull View target, int dxConsumed, int dyConsumed,
+                               int dxUnconsumed, int dyUnconsumed, int type) {
+
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.setVisibility(View.INVISIBLE);
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.animate().translationY(0);
+        }
+    }
+}
